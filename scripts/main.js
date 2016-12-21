944ffe4c95bd858ee3e49f2c41c3ae26ce0b5205
RAYMOND\Raymond Xu <ryx2@cornell.edu>
2016-12-21 00:01:35 -0500

    image switcher

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Raymond Xu';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/latte.png') {
+      myImage.setAttribute ('src','images/latte2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/latte.png');
+    }
+}
