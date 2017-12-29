409da406d14001b8d18550b4ce24395bd8b848a8
RAYMOND\Raymond Xu <ryx2@cornell.edu>
2017-12-29 17:53:34 -0500

    split pages for faster loading

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,27 @@
 jQuery.easing.def = "easeOutQuad";
 var loaded = "false";
-document.getElementById("hometab").style.marginLeft = "0";
-function openTab(evt, tabName) {
+var fromDefault = "false";
+function goToTab(evt, tabName){
+    window.location.href = `${tabName}.html`;
+}
+function openTab(tabName) {
+    if(tabName==="index"){
+        document.getElementById("hometab").style.marginLeft = "0";
+    }
     var i, tabcontent, tablinks;
     window.scrollTo(0,0);
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tab");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
 
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("menu");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
+    // tablinks = document.getElementsByClassName("menu");
+    // for (i = 0; i < tablinks.length; i++) {
+    //     tablinks[i].className = tablinks[i].className.replace(" active", "");
+    // }
+    // document.getElementById(tabName) //make this black
     // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-    if (tabName==='home') {
+    // document.getElementById(tabName).style.display = "block";
+
+    //evt.currentTarget.className += " active";
+    if (tabName==='index') {
         if(loaded === "false"){
             //only trigger animation when the css has loaded on the page
             // if (!$("link[href='../styles/style.css']").length){
@@ -34,13 +36,11 @@ function openTab(evt, tabName) {
             // link.href = "../styles/style.css";
             
             // link.onload = function () {
-            $(window).on("load",function(){
-                loaded = "true";
-                $("#hometab").delay(200).velocity({
-                    marginLeft: '200px',
-                    fontSize: '2.5em'
-                },2000,[400,20]); /*velocity's spring animation for a 1.2 seconds*/
-            });
+            loaded = "true";
+            $("#hometab").delay(400).velocity({
+                marginLeft: '200px',
+                fontSize: '2.5em'
+            },2000,[400,20]); /*velocity's spring animation for a 1.2 seconds*/
         } else {
             $("#hometab").velocity("stop");
             document.getElementById("hometab").style.marginLeft = "0";
@@ -66,8 +66,6 @@ function openTab(evt, tabName) {
         },{duration:500, loop:3});
     }; 
 }
-// open home tab by default
-document.getElementById("default").click();
 // slide open something
 function slideOpen(evt, sliding){
     var items;
